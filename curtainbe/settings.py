045ee9eb8ba97fc37629523aa4f9516a43af10ec
Toan Phung <toan.phung@proteo.info>
2023-11-11 22:34:10 +0000

    Update settings.py

--- curtainbe/settings.py
+++ curtainbe/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'request.middleware.RequestMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
