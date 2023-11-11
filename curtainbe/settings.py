03ce8c40d8576cda28ac931ce7e599252cad5186
Toan Phung <toan.phung@proteo.info>
2023-11-11 21:29:05 +0000

    Update settings.py

--- curtainbe/settings.py
+++ curtainbe/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.middleware.gzip.GZipMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
