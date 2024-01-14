a324fb43b85df4bffe6a648eabf7f4dcb633110c
Toan Phung <toan.phung@proteo.info>
2024-01-14 21:10:02 +0000

    update

--- curtainbe/settings.py
+++ curtainbe/settings.py
@@ -220,6 +220,40 @@ STORAGES = {
     },
 }
 
+
+REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
+REDIS_PORT = os.environ.get("REDIS_PORT", "6379")
+REDIS_DB = os.environ.get("REDIS_DB", "0")
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", "")
+REDIS_URL = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
+
+REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = (
+            "rest_framework.renderers.JSONRenderer",
+        )
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [REDIS_URL],
+            "symmetric_encryption_keys": [SECRET_KEY]
+        },
+    },
+}
+ORCID = {
+    'client_id': os.environ.get('ORCID_OAUTH_CLIENT_ID', 'resorc'),
+    'secret': os.environ.get('ORCID_OAUTH_SECRET', 'resorc'),
+}
+
+RQ_QUEUES = {
+    'default': {
+        'HOST': REDIS_HOST,
+        'PORT': REDIS_PORT,
+        'DB': REDIS_DB,
+        'PASSWORD': REDIS_PASSWORD,
+        'DEFAULT_TIMEOUT': 1440,
+    },
+}
+
 if os.environ.get("WORKING_ENV") == "PRODUCTION":
     DEBUG = os.environ.get("DEBUG", "False") == "True"
     SECRET_KEY = os.environ.get("SECRET_KEY")
@@ -279,38 +313,7 @@ if os.environ.get("WORKING_ENV") == "PRODUCTION":
         }
     }
 
-    REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
-    REDIS_PORT = os.environ.get("REDIS_PORT", "6379")
-    REDIS_DB = os.environ.get("REDIS_DB", "0")
-    REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", "")
-    REDIS_URL = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
 
-    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = (
-                "rest_framework.renderers.JSONRenderer",
-            )
-    CHANNEL_LAYERS = {
-        "default": {
-            "BACKEND": "channels_redis.core.RedisChannelLayer",
-            "CONFIG": {
-                "hosts": [REDIS_URL],
-                "symmetric_encryption_keys": [SECRET_KEY]
-            },
-        },
-    }
-    ORCID = {
-        'client_id': os.environ.get('ORCID_OAUTH_CLIENT_ID', 'resorc'),
-        'secret': os.environ.get('ORCID_OAUTH_SECRET', 'resorc'),
-    }
-
-    RQ_QUEUES = {
-        'default': {
-            'HOST': REDIS_HOST,
-            'PORT': REDIS_PORT,
-            'DB': REDIS_DB,
-            'PASSWORD': REDIS_PASSWORD,
-            'DEFAULT_TIMEOUT': 1440,
-        },
-    }
 
     if os.environ.get("CURTAIN_ALLOW_NON_STAFF_DELETE"):
         v = int(os.environ.get("CURTAIN_ALLOW_NON_STAFF_DELETE"))
