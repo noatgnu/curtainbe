bb098747f34509cec9f6fcd70addf6b20a265e71
Toan Phung <toan.phung@proteo.info>
2024-02-14 20:23:51 +0000

    Update authentication.py

--- curtain/authentication.py
+++ curtain/authentication.py
@@ -9,10 +9,10 @@ class APIKeyAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         key = request.META.get('HTTP_X_API_KEY')
         if not key:
-            raise exceptions.AuthenticationFailed('No API key provided.')
+            return None
         try:
             api_key = UserAPIKey.objects.get_from_key(key)
             user = api_key.user
         except User.DoesNotExist:
-            raise exceptions.AuthenticationFailed('Invalid API key.')
-        return (user, None)
+            return None
+        return user
